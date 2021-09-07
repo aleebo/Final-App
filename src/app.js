94b149a7421488085fe2c9511cb18a4ee88e5638
aleebo <87545942+aleebo@users.noreply.github.com>
2021-09-06 21:53:38 -0700

    Remove error cause by } in js

--- src/app.js
+++ src/app.js
@@ -88,12 +88,12 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-   iconElement.setAttribute(
+  iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-}
+
   getForecast(response.data.coord);
 }
 
